9bfac74871ab4cf70d26e7f9e97495cad56821d6
The Passionate Programmer <wamungai10@gmail.com>
2024-01-04 09:32:59 +0300

    Add HTML skeleton and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section className="showcase">
+      <div className="overlay">
+        <article>
+          <h1>Learning to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time.
+            Watching scripted tutorials is great, but understanding how developers think is invaluable.
+          </p>
+        </article>
+        <article>
+          <p className="tag">
+            <strong>Try it free 7 days</strong> then $20/mo. thereafter
+          </p>
+          <form className="form" >
+            <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            />
+             <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            />
+             <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            />
+             <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            />
+            <button type="submit"> Claim your free trial</button>
+            <small>By clicking the button, you are agreeing to our {" "}
+            <span>Terms and Services</span>
+            </small>
+          </form>
+        </article>
+      </div>
+      </section> 
+    </>
   );
 }
 
